207f9c3dd71823c3e6dc00cb982d9454bffdda7f
Marcel Gaupp <github@blankzero0.net>
2023-11-18 12:21:33 +0100

    Add main input page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from './check24.svg'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header className='bg-primary text-bg-primary py-2 px-4 mb-4'>
+        <h1 className='mx-4'>
+          <a href='/'><img src={logo} alt='CHECK24 Logo' height='24px' className='me-3' /></a>
+          Handwerkervergleich</h1>
       </header>
+      <main className='container'>
+        <p>In welcher PLZ suchen Sie einen Maler?</p>
+        <div className='form-floating'>
+          <input type='text' id='plz' inputMode='numeric' minLength={5} maxLength={5} autoComplete='postal-code' className='form-control mb-4'/>
+          <label htmlFor='plz' className='form-label'>Ihre PLZ</label>
+        </div>
+        <div>
+          <button className='btn btn-primary'>Weiter</button>
+        </div>
+      </main>
     </div>
   );
 }
