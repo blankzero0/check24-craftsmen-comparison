b23fb9b6f6d8e630622a33ae81e989f45c9899e9
Marcel Gaupp <github@blankzero0.net>
2023-11-18 12:24:51 +0100

    Use full-width button

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,15 +9,13 @@ function App() {
           <a href='/'><img src={logo} alt='CHECK24 Logo' height='24px' className='me-3' /></a>
           Handwerkervergleich</h1>
       </header>
-      <main className='container'>
-        <p>In welcher PLZ suchen Sie einen Maler?</p>
+      <main className='container d-grid gap-4'>
+        <p className='my-0'>In welcher PLZ suchen Sie einen Maler?</p>
         <div className='form-floating'>
-          <input type='text' id='plz' inputMode='numeric' minLength={5} maxLength={5} autoComplete='postal-code' className='form-control mb-4'/>
+          <input type='text' id='plz' inputMode='numeric' minLength={5} maxLength={5} autoComplete='postal-code' className='form-control'/>
           <label htmlFor='plz' className='form-label'>Ihre PLZ</label>
         </div>
-        <div>
-          <button className='btn btn-primary'>Weiter</button>
-        </div>
+        <button className='btn btn-primary'>Weiter</button>
       </main>
     </div>
   );
