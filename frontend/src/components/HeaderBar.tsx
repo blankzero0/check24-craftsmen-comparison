c3aa4721f5d2302c10de629d33ed9eb70bf18296
emil <ge94fuf@mytum.de>
2023-11-18 16:32:06 +0100

    New Button animation

--- frontend/src/components/HeaderBar.tsx
+++ frontend/src/components/HeaderBar.tsx
@@ -2,11 +2,19 @@ import React from 'react';
 import logo from "../check24.svg";
 
 function HeaderBar() {
+  const headerStyle = {
+    backgroundColor: '#063773',
+    color: 'white',
+    padding: '1rem 1rem',
+    marginBottom: '1rem'
+  };
+
   return (
-    <header className='bg-primary text-bg-primary py-2 px-4 mb-4'>
-      <h1 className='mx-4'>
+    <header className='text-white py-4 mb-2' style={{backgroundColor: '#063773'}}>
+      <h1 className='container mb-0'>
         <a href='/'><img src={logo} alt='CHECK24 Logo' height='24px' className='me-3' /></a>
-        Handwerkervergleich</h1>
+        <span className="header-title fs-3">Handwerkervergleich</span>
+      </h1>
     </header>
   );
 }
--- frontend/src/components/SearchForm.tsx
+++ frontend/src/components/SearchForm.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
+import './SearchForm.css'
 
 function SearchForm() {
+
+
   return (
     <main className='container d-grid gap-4'>
       <p className='my-0'>In welcher PLZ suchen Sie einen Maler?</p>
@@ -8,7 +11,7 @@ function SearchForm() {
         <input type='text' id='plz' inputMode='numeric' minLength={5} maxLength={5} autoComplete='postal-code' className='form-control'/>
         <label htmlFor='plz' className='form-label'>Ihre PLZ</label>
       </div>
-      <button className='btn btn-primary'>Weiter</button>
+      <button className='btn btn-primary py-3'>Weiter</button>
     </main>
   );
 }
