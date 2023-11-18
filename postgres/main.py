d13d09f2e7fdd7a98628937f26120566d671b943
Friedjof <friedjof@noweck.info>
2023-11-19 00:21:52 +0100

    fix: postgres

--- postgres/main.py
+++ postgres/main.py
@@ -6,8 +6,16 @@ from models.profile import Profile
 
 
 if __name__ == '__main__':
-    engine = create_engine('postgresql://craftmen:craftmen@localhost:5432/postgres')
+    engine = create_engine('postgresql://craftmen:craftmen@localhost:5432/craftmen')
     Session = sessionmaker(bind=engine)
     session = Session()
 
     # create tables
+    Postcode.__table__.create(engine)
+    Profile.__table__.create(engine)
+
+    # commit
+    session.commit()
+
+    # close session
+    session.close()
