d0a3261b657011855f22782920d1ed06d3406073
Marcel Gaupp <github@blankzero0.net>
2023-11-18 23:46:42 +0100

    Fix logo size

--- frontend/src/components/HeaderBar.tsx
+++ frontend/src/components/HeaderBar.tsx
@@ -3,17 +3,10 @@ import logo from "../check24.svg";
 import {Link} from "react-router-dom";
 
 function HeaderBar() {
-  const headerStyle = {
-    backgroundColor: '#063773',
-    color: 'white',
-    padding: '1rem 1rem',
-    marginBottom: '1rem'
-  };
-
   return (
     <header className='text-white py-4 mb-4' style={{backgroundColor: '#063773'}}>
       <h1 className='container mb-0'>
-        <Link to='/'><img src={logo} alt='CHECK24 Logo' height='24px' className='me-3' /></Link>
+        <Link to='/'><img src={logo} alt='CHECK24 Logo' width='98' height='24' className='me-3' /></Link>
         <span className="header-title fs-3">Handwerkervergleich</span>
       </h1>
     </header>
